6a44e8e4c24400ac575be5294d063b62038fc4b6
AdiadiAH <adityaram2027ca@gmail.com>
2025-05-02 23:26:17 +0000

    Iteration of machine communication between client and only the last connected client

--- ChatClient.py
+++ ChatClient.py
@@ -1,31 +1,34 @@
 import socket
+import sys
 import select
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-address = ("127.0.0.1", 0)
-try:
-    sock.bind(address)
-except Exception as e:
-    print("Issue connecting; Check")
-    print(e)
-    pass
-sock.connect(("127.0.0.1", 10000))
-try:
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.connect(("192.168.56.2", 10000))
+    sock.sendall(name.encode('utf-8'))
+    print(f"Chat Window is Ready for {name}")
+    inputs = [sys.stdin, sock]
+    outputs = [sys.stdout, sock]
+    errors = [sock]
     while True:
-        message = input("Send Message\n")
-        if message.lower().strip() == "close":
-            break
-        print(f"sending {message}")
-        sock.sendall(message.encode('utf-8'))
-        amount_received = 0
-        amount_expected = len(message)
-        
-        while amount_received < amount_expected:
-            data = sock.recv(1024)
-            amount_received += len(data)
-            print(f"received {data}")
-            
-            
-finally:
-    print("closing socket")
-    sock.close()
+        #print(f"{inputs=}, {outputs=}, {errors=}\n")
+        readable, writable, exceptional = select.select(inputs, outputs, errors)
+        for s in readable:
+            if s is sock:
+                data = s.recv(1024)
+                if data:
+                    print(f"{' '*10}{data.decode()}")
+                else:
+                    inputs.remove(s)
+                    outputs.remove(s)
+                    errors.remove(s)
+                    print ("The connection is closed")
+                    s.close()
+            else:
+                message = sys.stdin.readline().strip()
+                if message:
+                    sock.sendall(message.encode('utf-8'))
+
+else:
+    print("Please Provide a Username")
